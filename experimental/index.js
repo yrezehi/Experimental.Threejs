52b9541dfe9fd1d0d19c277d8d313c71f565eb24
unknown <yrezehi@gmail.com>
2023-11-03 07:30:41 -0700

    sync

--- experimental/index.js
+++ experimental/index.js
@@ -2,26 +2,18 @@
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 ); 
     
-    camera.position.set( 0, 0, 100 );
-    camera.lookAt( 0, 0, 0 );
-
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight, false);
     
     document.body.appendChild(renderer.domElement);
 
-    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    // base unrealated    
 
-    const points = [];
-    points.push( new THREE.Vector3( - 20, 0, 0 ) );
-    points.push( new THREE.Vector3( 20, 10, 0 ) );
-    points.push( new THREE.Vector3( 20, 0, 0 ) );
-    
-    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    new THREE.GLTFLoader().load( '../assets/zeldas_moon/scene.gltf', function ( gltf ) {
+        scene.add( gltf.scene );
+    }, undefined, function ( error ) {
+        console.error( error );
+    } );
 
-    const line = new THREE.Line( geometry, material );
-
-    scene.add( line );
     renderer.render( scene, camera );
-
 })();
---
+++ util/threejs.util.js
@@ -0,0 +1,6 @@
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render();
+}, false)
