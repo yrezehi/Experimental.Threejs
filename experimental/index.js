61cf81bf18512ecf39a1947b841f78eec6a30e85
unknown <yrezehi@gmail.com>
2023-11-03 08:08:17 -0700

    sync

--- experimental/index.js
+++ experimental/index.js
@@ -1,19 +1,37 @@
 (function () {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 ); 
-    
+    scene.background = new THREE.Color( 0xF2F2F2 );
+
+    var camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, .1, 10000 );
+    camera.position.set(0, 0, 300);
+
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight, false);
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 1;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
     document.body.appendChild(renderer.domElement);
 
     // base unrealated    
 
-    new THREE.GLTFLoader().load( '../assets/zeldas_moon/scene.gltf', function ( gltf ) {
+    new THREE.GLTFLoader().load( '../assets/zeldas_moon/bin/scene.glb', function ( gltf ) {
         scene.add( gltf.scene );
     }, undefined, function ( error ) {
         console.error( error );
     } );
 
-    renderer.render( scene, camera );
+    function animate() {
+        render();
+        requestAnimationFrame( animate );
+    }
+    
+    function render() {
+        renderer.render( scene, camera );
+    }
+
+    animate();
 })();
