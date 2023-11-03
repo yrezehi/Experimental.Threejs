9602eb2ea6efa014ba67d46a188fda9ce2694842
unknown <yrezehi@gmail.com>
2023-11-03 08:55:46 -0700

    sync

--- experimental/index.js
+++ experimental/index.js
@@ -1,36 +1,46 @@
 (function () {
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xF2F2F2 );
+    scene.background = new THREE.Color(0xF2F2F2);
 
-    var camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, .1, 10000 );
+    const ambientLight = new THREE.AmbientLight(0x555555);
+    ambientLight.intensity = 4;
+    scene.add(ambientLight);
+
+    var camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, .1, 300);
     camera.position.set(0, 0, 300);
 
     const renderer = new THREE.WebGLRenderer();
 
-    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    
+
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = 1;
     renderer.outputEncoding = THREE.sRGBEncoding;
+
+    const controls = new THREE.OrbitControls( camera, renderer.domElement );
+    
+    controls.enableZoom = true;
+    controls.enabled = true;
+    controls.target.set(0, 0, 0);
 
     document.body.appendChild(renderer.domElement);
 
     // base unrealated    
 
-    new THREE.GLTFLoader().load( '../assets/zeldas_moon/bin/scene.glb', function ( gltf ) {
-        scene.add( gltf.scene );
-    }, undefined, function ( error ) {
-        console.error( error );
-    } );
+    new THREE.GLTFLoader().load('../assets/zeldas_moon/bin/scene.glb', function (gltf) {
+        scene.add(gltf.scene);
+    }, undefined, function (error) {
+        console.error(error);
+    });
 
     function animate() {
+        controls.update();
         render();
-        requestAnimationFrame( animate );
+        requestAnimationFrame(animate);
     }
-    
+
     function render() {
-        renderer.render( scene, camera );
+        renderer.render(scene, camera);
     }
 
     animate();
