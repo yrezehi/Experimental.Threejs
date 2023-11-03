69ea09e7481ee4dd1856fd41afe20d1d5874a6d0
unknown <yrezehi@gmail.com>
2023-11-03 10:51:48 -0700

    sync

--- experimental/index.js
+++ experimental/index.js
@@ -6,27 +6,37 @@
     ambientLight.intensity = 4;
     scene.add(ambientLight);
 
-    var camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, .1, 500 );
+    var camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, .1, 500);
     camera.position.set(0, 0, 300);
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setPixelRatio(window.devicePixelRatio);
 
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = 1;
     renderer.outputEncoding = THREE.sRGBEncoding;
 
-    const controls = new THREE.OrbitControls( camera, renderer.domElement );
-    
-    controls.enableZoom = true;
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    controls.enableRotate = false;
+    controls.enablePan = false;
+    controls.enableZoom = false;
     controls.enabled = true;
+    controls.enableDamping = true;
+    controls.minPolarAngle = 0.8;
+    controls.maxPolarAngle = 2.4;
+    controls.dampingFactor = 0.07;
+    controls.rotateSpeed = 0.07;
     controls.target.set(0, 0, 0);
 
     document.body.appendChild(renderer.domElement);
 
-    // base unrealated    
+    document.addEventListener('mousemove', function(event) {
+        console.log(controls);
+        controls.handleMouseMoveRotate(event);
+    }, false);
 
     new THREE.GLTFLoader().load('../assets/zeldas_moon.glb', function (gltf) {
         scene.add(gltf.scene);
